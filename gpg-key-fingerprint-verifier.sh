f307bfd00150c79c70fa43746c96fd9690972c5a
Akai <akai-z@users.noreply.github.com>
2018-11-03 20:25:04 +0200

    Add GPG version function

--- gpg-key-fingerprint-verifier.sh
+++ gpg-key-fingerprint-verifier.sh
@@ -4,3 +4,11 @@ set -Eeuo pipefail
 
 readonly VERSION_LABEL="gpg"
 readonly REQUIRED_DEPS=("gpg")
+
+gpg_version() {
+  echo "$( \
+    gpg --version \
+      | grep -0 "^${VERSION_LABEL}" \
+      | cut -d " " -f 3 \
+  )"
+}
