1950f115ded58cca62458af287e3d4c0ce42f3c7
Akai <akai-z@users.noreply.github.com>
2018-11-03 20:36:04 +0200

    Add dependencies check function

--- gpg-key-fingerprint-verifier.sh
+++ gpg-key-fingerprint-verifier.sh
@@ -13,6 +13,17 @@ gpg_version() {
   )"
 }
 
+deps_check() {
+  local dep
+
+  for dep in "${REQUIRED_DEPS[@]}"
+  do
+    if [ ! -x "$(command -v "$dep")" ]; then
+      error "$(printf "Command \"%s\" was not found." "$dep")"
+    fi
+  done
+}
+
 error() {
   echo -e >&2 "\n$1\n"
   exit 1
